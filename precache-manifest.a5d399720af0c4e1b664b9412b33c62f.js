88dbe87fc99d53a88607b4af7dbc177d5a26ea58
marcywebdev <marcelinojasso@hotmail.com>
2020-05-19 18:10:13 -0700

    Updates

---
+++ precache-manifest.a5d399720af0c4e1b664b9412b33c62f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10276c1b7356d3e392503aaca97b9a9a",
+    "url": "/marcywebdev/projectreact.git/index.html"
+  },
+  {
+    "revision": "1353b56c68855391fbcc",
+    "url": "/marcywebdev/projectreact.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cac834ef738db2a548dd",
+    "url": "/marcywebdev/projectreact.git/static/js/2.fbeda752.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marcywebdev/projectreact.git/static/js/2.fbeda752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1353b56c68855391fbcc",
+    "url": "/marcywebdev/projectreact.git/static/js/main.275e9d1f.chunk.js"
+  },
+  {
+    "revision": "e2a31d1425d0b0abdf1a",
+    "url": "/marcywebdev/projectreact.git/static/js/runtime-main.3f3af59d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marcywebdev/projectreact.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
